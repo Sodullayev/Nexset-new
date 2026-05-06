ec1b2c37fdb72feaca647469cd0396a55b13c500
Javohir <sodullayevjavohir14@gmail.com>
2026-05-06 18:05:56 +0500

    yangi o'zgarish qo'shildi

--- main.js
+++ main.js
@@ -42,3 +42,53 @@ current = 0;
 }
 changeSlide();
 }, 3000);
+
+
+let signupBtn = document.querySelector(".signup");
+let modal = document.querySelector("#modal");
+let modalClose = document.getElementById("modalClose");
+
+signupBtn.addEventListener("click" ,   function (e) {
+  e.preventDefault();
+  modal.classList.add("show-modal");
+});
+
+modalClose.addEventListener("click", function () {
+  modal.classList.remove("show-modal");
+});
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    modal.classList.remove("show-modal");
+  }
+});
+
+
+
+
+let loginBtn = document.querySelector(".login");
+let loginmodal = document.querySelector("#loginModal");
+let loginModalClose = document.getElementById("loginClose");
+
+loginBtn.addEventListener("click" ,   function (e) {
+  e.preventDefault();
+  loginmodal.classList.add("show-modal");
+});
+
+loginModalClose.addEventListener("click", function () {
+  loginmodal.classList.remove("show-modal");
+});
+
+loginmodal.addEventListener("click", function (e) {
+  if (e.target === loginmodal) {
+    loginmodal.classList.remove("show-modal");
+  }
+});
+
+let goSignup = document.getElementById("loginSignup");
+
+goSignup.addEventListener("click", function (e) {
+  e.preventDefault();
+  loginmodal.classList.remove("show-modal");
+  modal.classList.add("show-modal");
+});
