0decbadfe6bb75311b17428fb731a2cf22160e00
Javohir <sodullayevjavohir14@gmail.com>
2026-05-11 18:14:01 +0500

    Xatolar to'g'irlandi

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ closeBtn.addEventListener("click", function () {
   menu.classList.remove("show-menu");
 });
 
-// ===== CAROUSEL =====
+
 let carouselInner = document.querySelector(".carousel__inner");
 let dots = document.querySelectorAll(".dot");
 let current = 0;
 
-// Slaydni chiqarish funksiyasi
+
 function changeSlide() {
   carouselInner.style.transform = `translateX(-${current * 100}%)`;
 
@@ -27,7 +27,7 @@ function changeSlide() {
   dots[current].classList.add("active");
 }
 
-// Nuqta bosilganda
+
 dots.forEach(function (dot, index) {
   dot.addEventListener("click", function () {
     current = index;
